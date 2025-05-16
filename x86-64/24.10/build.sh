568d3b10bf24f826d72a92e72e7c23d4a9f7bb09
WoChen5770 <i@ichenqiang.com>
2025-05-16 23:11:17 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -45,8 +45,8 @@ PACKAGES="$PACKAGES luci-nginx"
 #PACKAGES="$PACKAGES script-utils"
 #PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 
-#PACKAGES="$PACKAGES app-meta-rtbwmon"
-#PACKAGES="$PACKAGES luci-i18n-rtbwmon-zh-cn"
+PACKAGES="$PACKAGES app-meta-rtbwmon"
+PACKAGES="$PACKAGES luci-i18n-rtbwmon-zh-cn"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
